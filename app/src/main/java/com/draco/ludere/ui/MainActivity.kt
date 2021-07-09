54459600084b35d50bba17abb7da36b9aaca9e32
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-09 11:32:50 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         setContentView(R.layout.activity_main)
 
     }
