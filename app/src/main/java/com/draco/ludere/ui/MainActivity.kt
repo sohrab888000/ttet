cdc572861489136a18034e83ee639cd179ac05af
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-09 03:12:20 -0700

    Add files via upload

---
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.draco.ludere.views
+import android.widget.RelativeLayout
+import java.util.*
+import kotlin.system.exitProcess
+import android.net.Uri
+import android.content.Intent
+import android.app.Activity
+import android.app.Service
+import android.content.DialogInterface
+import android.hardware.input.InputManager
+import android.os.Build
+import android.os.Bundle
+import android.view.*
+import android.widget.FrameLayout
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.draco.ludere.R
+import com.draco.ludere.gamepad.GamePad
+import com.draco.ludere.gamepad.GamePadConfig
+import com.draco.ludere.input.ControllerInput
+import com.draco.ludere.retroview.RetroView
+import com.draco.ludere.utils.RetroViewUtils
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+    /** Called when the user touches the button */
+    fun start(view: View) {
+        // Do something in response to button click
+        val start_the_game_button = findViewById(R.id.start_the_game_button) as Button
+        start_the_game_button.isEnabled = false
+        start_the_game_button.visibility = View.INVISIBLE
+        val comments = findViewById(R.id.comments) as Button
+        comments.isEnabled = false
+        comments.visibility = View.INVISIBLE
+        val game_page = findViewById(R.id.game_page) as Button
+        game_page.isEnabled = false
+        game_page.visibility = View.INVISIBLE
+        val exit_button = findViewById(R.id.exit_button) as Button
+        exit_button.isEnabled = false
+        exit_button.visibility = View.INVISIBLE
+        val send_email = findViewById(R.id.send_email) as Button
+        send_email.isEnabled = false
+        send_email.visibility = View.INVISIBLE
+        val relative = findViewById(R.id.relative) as RelativeLayout
+        relative.setBackgroundResource(0)
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun sendMsg(view: View) {
+        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.chavez2")
+        startActivity(openURL)
+    }
+
+    fun sendingEmail(view: View) {
+
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto: siavashiranpak@gmail.com")
+        intent.putExtra(Intent.EXTRA_SUBJECT, "نظر دهی")
+        startActivity(intent)
+
+    }
+
+    fun goToPage(view: View) {
+        val openURL = Intent(android.content.Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.chavez2")
+        startActivity(openURL)
+    }
+
+    fun exit_game(view: View) {
+        this@MainActivity.finish()
+        exitProcess(0)
+    }
+
+}
