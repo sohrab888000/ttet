2a67e883db4693853b80d4934705be62eb066c15
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-09 04:03:27 -0700

    Update GameActivity.kt

--- app/src/main/java/com/draco/ludere/ui/GameActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/GameActivity.kt
@@ -313,10 +313,10 @@ class GameActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        if (retroView != null)
-            Menu(this, retroView!!).show()
-    }
+  //  override fun onBackPressed() {
+  //      if (retroView != null)
+  //          Menu(this, retroView!!).show()
+  //  }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         return input.handleKeyEvent(retroView, keyCode, event) || super.onKeyDown(keyCode, event)
