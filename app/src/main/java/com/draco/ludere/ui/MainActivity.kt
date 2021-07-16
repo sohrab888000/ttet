7ed01a2dbd382d83a1243f108cc69d025655ea1a
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-16 06:38:33 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.draco.ludere.ui
+import java.io.*
+import java.util.zip.ZipFile
 import java.io.File
 import java.io.InputStream
 import java.io.OutputStream
@@ -43,20 +45,14 @@ class MainActivity : AppCompatActivity() {
     val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
     val afile = assets.open( "example.n64" )
     val bfile = File(storagePath + "/example.n64")
-    var inStream: InputStream? = null
-    var outStream: OutputStream? = null
-    inStream = afile
-    outStream = FileOutputStream(bfile)
-    val buffer = ByteArray(1024)
-    var length = inStream.read(buffer)
-    while (length    > 0 )
-    {
-        outStream.write(buffer, 0, length)
-        length = inStream.read(buffer)
-    }
-    inStream.close()
-    outStream.close()
+        var fileExists = bfile.exists()
+    if(fileExists){
 
+    } else {
+
+        unzip(afile, storagePath)
+        
+    }
         /*
         new-> just copying
         */
@@ -116,5 +112,47 @@ class MainActivity : AppCompatActivity() {
         this@MainActivity.finish()
         exitProcess(0)
     }
+    
+    
+        fun unzip(zipFilePath: File, destDirectory: String) {
+        val destDir = File(destDirectory)
+        if (!destDir.exists()) {
+            destDir.mkdir()
+        }
+        ZipFile(zipFilePath).use { zip ->
+
+            zip.entries().asSequence().forEach { entry ->
+
+                zip.getInputStream(entry).use { input ->
+
+
+                        val filePath = destDirectory + File.separator + entry.name
+
+                        if (!entry.isDirectory) {
+                            // if the entry is a file, extracts it
+                            extractFile(input, filePath)
+                        } else {
+                            // if the entry is a directory, make the directory
+                            val dir = File(filePath)
+                            dir.mkdir()
+                        }
+
+                }
+
+            }
+        }
+    }
+    
+        private fun extractFile(inputStream: InputStream, destFilePath: String) {
+        val bos = BufferedOutputStream(FileOutputStream(destFilePath))
+        val bytesIn = ByteArray(BUFFER_SIZE)
+        var read: Int
+        while (inputStream.read(bytesIn).also { read = it } != -1) {
+            bos.write(bytesIn, 0, read)
+        }
+        bos.close()
+    }
+        
+        
 
 }
