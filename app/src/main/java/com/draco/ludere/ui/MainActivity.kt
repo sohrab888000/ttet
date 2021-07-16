42f8ee0c58eb8f722304918181471cb6f13774e9
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-16 04:01:18 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -1,4 +1,8 @@
 package com.draco.ludere.ui
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.FileOutputStream
 import android.widget.RelativeLayout
 import java.util.*
 import kotlin.system.exitProcess
@@ -33,6 +37,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+                       /*
+        new-> just copying
+        */
+    val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
+    val afile = assets.open( "example.gba" )
+    val bfile = File(storagePath + "/example.gba")
+    var inStream: InputStream? = null
+    var outStream: OutputStream? = null
+    inStream = afile
+    outStream = FileOutputStream(bfile)
+    val buffer = ByteArray(1024)
+    var length = inStream.read(buffer)
+    while (length    > 0 )
+    {
+        outStream.write(buffer, 0, length)
+        length = inStream.read(buffer)
+    }
+    inStream.close()
+    outStream.close()
+
+        /*
+        new-> just copying
+        */
+        
+        
+        
     }
 
 
