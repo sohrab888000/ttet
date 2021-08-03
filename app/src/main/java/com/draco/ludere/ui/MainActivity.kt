0137a1b7c29cec4708fc3b3e823aaf32926a40c6
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-08-02 18:11:03 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity() {
         
 			
         val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
-        val cfile = File(storagePath + "/Captain Tsubasa.cue")//diffrent for each game
+        val cfile = File(storagePath + "/cvs.dat")//diffrent for each game
         var fileExists = cfile.exists()
         val bfile = File(storagePath + "/example.zip")
         var fileExistscheck = bfile.exists()
