3e5d6ad8a621faf7fc678b43665ea778bd9213a0
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-08-09 18:23:15 -0700

    Create App.kt

---
+++ app/src/main/java/com/draco/ludere/App.kt
@@ -0,0 +1,12 @@
+package com.draco.ludere
+
+import ir.tapsell.sdk.Tapsell
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Tapsell.initialize(this,"cgnilqogejklctgoegobtnnfrtnkbknmfqqkqbotdslosaidartitmlootigbhfphjltia")//insert key of each application here
+    }
+}
