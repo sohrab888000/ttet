37623a33dfcf9012c7dadbd9d7ca6d933a709ebd
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-08-14 04:18:34 -0700

    Update GameActivity.kt

--- app/src/main/java/com/draco/ludere/ui/GameActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/GameActivity.kt
@@ -133,7 +133,7 @@ class GameActivity : AppCompatActivity() {
         val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
         val retroViewData = GLRetroViewData(this).apply {
             coreFilePath = "libcore.so" 
-            gameFilePath = "$storagePath/Tekken - Dark Resurrection.iso"
+            gameFilePath = "$storagePath/example.iso"
             gameFileBytes = null
             /* (Optional) System directory */
             systemDirectory = storagePath + "/system"
