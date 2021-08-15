23842a2f970e7cfc0be91299a55a6aec81b51205
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-08-15 03:41:28 -0700

    Update App.kt

--- app/src/main/java/com/draco/ludere/App.kt
+++ app/src/main/java/com/draco/ludere/App.kt
@@ -7,6 +7,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Tapsell.initialize(this,"rtfkkdrotifjrkmsgbbfbblisaooiseonmskmbjbcncdtpghjgmltqbiaefresttarhhqj")//insert key of each application here
+        Tapsell.initialize(this,"roofdrpjhqgfbrbhonpdbjgqpaqgjeskaigbobjhomhgecmlrnpmtmrjgltccrqbqfpsqm")//insert key of each application here
     }
 }
