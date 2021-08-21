905715ee5b8ecab455f825fe2c37fab0df500b98
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-08-20 22:15:13 -0700

    Update InterstitialActivity.kt

--- app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
@@ -38,13 +38,13 @@ class InterstitialActivity : AppCompatActivity() {
                 val options = TapsellAdRequestOptions(CACHE_TYPE_STREAMED)
         Tapsell.requestAd(this@InterstitialActivity,
             //if (type == AdType.BANNER) "610ed8bc35114c6ff3a596ee" else
-                "611433c8841c9949037f3480", options,
+                "61208aa87a287273220e4c39", options,
             object : TapsellAdRequestListener() {
                 override fun onAdAvailable(ad: String) {
 
            val showOptions = TapsellShowOptions()
             showOptions.rotationMode = TapsellShowOptions.ROTATION_LOCKED_LANDSCAPE
-            Tapsell.showAd(this@InterstitialActivity, "611433c8841c9949037f3480" , ad , showOptions, object : TapsellAdShowListener() {
+            Tapsell.showAd(this@InterstitialActivity, "61208aa87a287273220e4c39" , ad , showOptions, object : TapsellAdShowListener() {
                 override fun onOpened(ad: TapsellAd) {
                     Log.e("InterstitialActivity", "on ad opened")
                 }
