bbcfee44b769ac76b19ec1d4c1199ea221133b57
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-08-25 19:45:00 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -86,7 +86,7 @@ f2.mkdir()
     fun start(view: View) {
 	    
 	   val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
-        val cfile = File(storagePath + "/example.iso")//diffrent for each game
+        val cfile = File(storagePath + "/example.cso")//diffrent for each game
         var fileExists = cfile.exists()
     val bfile = File(storagePath + "/system/PPSSPP/example.zip")
         var fileExistscheck = bfile.exists()
@@ -141,13 +141,13 @@ f2.mkdir()
     fun sendMsg(view: View) {
 	
 	/*myket*/
-	//val openURL = Intent(android.content.Intent.ACTION_VIEW)	    
-        //openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.socombravo")
+	val openURL = Intent(android.content.Intent.ACTION_VIEW)	    
+        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.TEKKEN5")
         
 	/*bazar*/
-	val openURL = Intent(android.content.Intent.ACTION_EDIT)
-        openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.socombravo")
-        openURL.setPackage("com.farsitel.bazaar")
+	//val openURL = Intent(android.content.Intent.ACTION_EDIT)
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.TEKKEN5")
+        //openURL.setPackage("com.farsitel.bazaar")
 	
 	
         startActivity(openURL)
@@ -159,9 +159,9 @@ f2.mkdir()
         
 	    
 	/*myket*/
-	//intent.data = Uri.parse("mailto: siavashiranpak@gmail.com")
+	intent.data = Uri.parse("mailto: siavashiranpak@gmail.com")
         /*bazar*/
-        intent.data = Uri.parse("mailto: 00sohrabiranpak00@gmail.com")        
+        //intent.data = Uri.parse("mailto: 00sohrabiranpak00@gmail.com")        
 	
 	    
 	    
@@ -177,11 +177,11 @@ f2.mkdir()
 	
 	
 	/*myket*/
-        //openURL.data = Uri.parse("myket://details?id=com.draco.ludere.socombravo")
+        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.TEKKEN5")
 	
 	/*bazar*/
-        openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.socombravo")
-        openURL.setPackage("com.farsitel.bazaar")
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.TEKKEN5")
+        //openURL.setPackage("com.farsitel.bazaar")
 	    
 	    
 	startActivity(openURL)
@@ -229,9 +229,9 @@ f2.mkdir()
 	var prev_download : Double = -1.0
 		val storagePath: String = (context.getExternalFilesDir(null) ?: context.filesDir).path             
 			var ll = 7816696 
-		        var ll2 = 105534208 
+		        var ll2 = 750534208 
 		        var ll_zip = 7816696
-	            	var ll_zip2 = 250534208
+	            	var ll_zip2 = 750534208
 		
 		
 	 	         var ll_download = 1100000000
