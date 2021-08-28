6ae3d6fefc586774dc9c8e059eb66a105d5bc918
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-08-27 23:13:29 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -142,11 +142,11 @@ f2.mkdir()
 	
 	/*myket*/
 	val openURL = Intent(android.content.Intent.ACTION_VIEW)	    
-        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.spidermanWebOfShadows")
+        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.pirateTwo")
         
 	/*bazar*/
 	//val openURL = Intent(android.content.Intent.ACTION_EDIT)
-        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.spidermanWebOfShadows")
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.pirateTwo")
         //openURL.setPackage("com.farsitel.bazaar")
 	
 	
@@ -177,10 +177,10 @@ f2.mkdir()
 	
 	
 	/*myket*/
-        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.spidermanWebOfShadows")
+        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.pirateTwo")
 	
 	/*bazar*/
-        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.spidermanWebOfShadows")
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.pirateTwo")
         //openURL.setPackage("com.farsitel.bazaar")
 	    
 	    
@@ -229,9 +229,9 @@ f2.mkdir()
 	var prev_download : Double = -1.0
 		val storagePath: String = (context.getExternalFilesDir(null) ?: context.filesDir).path             
 			var ll = 7816696 
-		        var ll2 = 225534208 
+		        var ll2 = 425534208 
 		        var ll_zip = 7816696
-	            	var ll_zip2 = 417534208
+	            	var ll_zip2 = 717534208
 		
 		
 	 	         var ll_download = 1100000000
