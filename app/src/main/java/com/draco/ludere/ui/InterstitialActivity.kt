1d79bbbbc94503e5e515fed7887a084cbb823894
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-09-02 19:25:30 -0700

    Update InterstitialActivity.kt

--- app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
@@ -35,7 +35,7 @@ class InterstitialActivity : AppCompatActivity() {
 //setContentView(Constraint)
 
         TapsellPlus.requestInterstitialAd(
-                this@InterstitialActivity, "612eb0957adba5677fb9fe2e",
+                this@InterstitialActivity, "6131866f66768f46ced024af",
                 object : AdRequestCallback() {
                    
                     override fun response(tapsellPlusAdModel : TapsellPlusAdModel) {
