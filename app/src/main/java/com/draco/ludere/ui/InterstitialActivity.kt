b26a1e3d2986fdfc01986440b55938760d4e3367
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-09-13 18:56:50 -0700

    Update InterstitialActivity.kt

--- app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
@@ -14,6 +14,7 @@ import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatButton
 import kotlinx.android.synthetic.main.activity_interstitial.*
 import android.content.Intent
+import java.io.File
 
 class InterstitialActivity : AppCompatActivity() {
 
