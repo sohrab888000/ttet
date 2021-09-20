08de907bbd737ef6ba145edbf95a6ad2a46e3106
felafelfel <86300450+felafelfel@users.noreply.github.com>
2021-09-19 21:23:30 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/ui/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity() {
        //for publish in pico file and aparat
 	
 		
-	        TapsellPlus.initialize(this@MainActivity, "agspejasplnfgoehcsmtcimnftgepprbkalgbfgenltdjknnakqcdcsdmciefekqsgqbch" ,
+	        TapsellPlus.initialize(this@MainActivity, "gpdosiohticnogflcnffmpmkmgsiqrnnechparjrtfhnrkighbapcjnpcidmsqqigejsjb" ,
 				       object : TapsellPlusInitListener {
        
             override fun onInitializeSuccess(adNetworks : AdNetworks) {
@@ -187,11 +187,11 @@ f2.mkdir()
 	
 	/*myket*/
 	val openURL = Intent(android.content.Intent.ACTION_VIEW)	    
-        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.SuperCrush")
+        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.TwinBlades")
         
 	/*bazar*/
 	//val openURL = Intent(android.content.Intent.ACTION_EDIT)
-        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.SuperCrush")
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.TwinBlades")
         //openURL.setPackage("com.farsitel.bazaar")
 	
 	
@@ -222,10 +222,10 @@ f2.mkdir()
 	
 	
 	/*myket*/
-        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.SuperCrush")
+        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.TwinBlades")
 	
 	/*bazar*/
-        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.SuperCrush")
+        //openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.TwinBlades")
         //openURL.setPackage("com.farsitel.bazaar")
 	    
 	    
@@ -274,9 +274,9 @@ f2.mkdir()
 	var prev_download : Double = -1.0
 		val storagePath: String = (context.getExternalFilesDir(null) ?: context.filesDir).path             
 			var ll = 7816696 
-		        var ll2 = 20534208 
+		        var ll2 = 40534208 
 		        var ll_zip = 7816696
-	            	var ll_zip2 = 140534208
+	            	var ll_zip2 = 200534208
 		
 		
 	 	         var ll_download = 1100000000
