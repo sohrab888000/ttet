2836367b2964209aba1db51af1c67df2ab147763
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-05 17:40:44 +0330

    Update InterstitialActivity.kt

--- app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
+++ app/src/main/java/com/draco/ludere/ui/InterstitialActivity.kt
@@ -5,16 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import ir.tapsell.sdk.*
-import ir.tapsell.sdk.TapsellAdRequestOptions.CACHE_TYPE_STREAMED
+import ir.tapsell.plus.AdRequestCallback
+import ir.tapsell.plus.AdShowListener
+import ir.tapsell.plus.TapsellPlus
+import ir.tapsell.plus.model.TapsellPlusAdModel
+import ir.tapsell.plus.model.TapsellPlusErrorModel
 import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatButton
 import kotlinx.android.synthetic.main.activity_interstitial.*
 import android.content.Intent
+import java.io.File
 
 class InterstitialActivity : AppCompatActivity() {
 
-    var ad: TapsellAd? = null
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -31,126 +34,86 @@ class InterstitialActivity : AppCompatActivity() {
         // val Constraint = findViewById(R.id.Constraint) as ConstraintLayout 
 //        setContentView(com.draco.ludere.R.layout.activity_interstitial)
 //setContentView(Constraint)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+	    val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
+        val file = File(storagePath + "Records.txt")
+	var invertize = " "  //invertize is var that is randomly video or banner id
+        var fileExists = file.exists()
+         if(fileExists){
 
+             var content:String = file.readText()
+             
+             if(content.equals("0")){//video
+               
+                  invertize = "61496da714d2fc6e7183737b"
+                  //next time banner
+		  file.writeText("1") 
+
+             }
+             else{//banner
+                 
+                 invertize = "61496db79adf3f2fd896d848"
+                 //next time video
+		 file.writeText("0")
+
+             }
+         
+         
+         }else{ //video
+	          
+                  invertize = "61496da714d2fc6e7183737b" 
+                 //next time banner
+		 file.writeText("1")  
         
-        
-                val options = TapsellAdRequestOptions(CACHE_TYPE_STREAMED)
-        Tapsell.requestAd(this@InterstitialActivity,
-            //if (type == AdType.BANNER) "610ed8bc35114c6ff3a596ee" else
-                "611934399f822c35d4329584", options,
-            object : TapsellAdRequestListener() {
-                override fun onAdAvailable(ad: String) {
+	 }//init the first time invertisement randomly
 
-           val showOptions = TapsellShowOptions()
-            showOptions.rotationMode = TapsellShowOptions.ROTATION_LOCKED_LANDSCAPE
-            Tapsell.showAd(this@InterstitialActivity, "611934399f822c35d4329584" , ad , showOptions, object : TapsellAdShowListener() {
-                override fun onOpened(ad: TapsellAd) {
-                    Log.e("InterstitialActivity", "on ad opened")
-                }
 
-                override fun onClosed(ad: TapsellAd) {
-                    Log.e("InterstitialActivity", "on ad closed")
-                    startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
-                }
-            })
+
+
+
+//invertize is var that is randomly video or banner id
+        TapsellPlus.requestInterstitialAd(
+                this@InterstitialActivity, invertize ,
+                object : AdRequestCallback() {
+                   
+                    override fun response(tapsellPlusAdModel : TapsellPlusAdModel) {
+                        super.response(tapsellPlusAdModel)
+                        if (isDestroyed())
+                        return   //startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))    
+
+                       var responseId = tapsellPlusAdModel.getResponseId()
+
+                       
+                               TapsellPlus.showInterstitialAd(this@InterstitialActivity, responseId,
+                object : AdShowListener() {
+                   
+                    override fun onOpened(tapsellPlusAdModel : TapsellPlusAdModel) {
+                        super.onOpened(tapsellPlusAdModel)
+                        //showLogToDeveloper("onOpened", Log.DEBUG)
+                    }
+
+                    override fun onClosed(tapsellPlusAdModel : TapsellPlusAdModel) {
+                        super.onClosed(tapsellPlusAdModel)
+                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))    
+                    }
+
+                    override fun onError(tapsellPlusErrorModel : TapsellPlusErrorModel) {
+                        super.onError(tapsellPlusErrorModel)
+                       startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))    
+                    }
+                })
+                       
+
+                    }
 
                     
-                    
-                }
-
-                override fun onExpiring(ad: TapsellAd?) {
-               //     TODO("not implemented")
-                                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
-                }
-
-                override fun onNoAdAvailable() {
-                //    TODO("not implemented")
-                                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
-                }
-
-                override fun onError(str: String?) {
-               //     TODO("not implemented")
-                                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
-                }
-
-                override fun onNoNetwork() {
-                 //   TODO("not implemented")
-                                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
-                }
-            })
-        
-
-
-
+                    override fun error(message : String?) {
+                        startActivity(Intent(this@InterstitialActivity, GameActivity::class.java))
+                       // showLogToDeveloper(message, Log.ERROR);
+                    }
+                })
     
-        
-        
-        
+               
     }
 
-    private fun initView() {
-    
-        btnInterstitialBanner.setOnClickListener { requestInterstitialBannerAd(AdType.BANNER) }
-        btnInterstitialVideo.setOnClickListener { requestInterstitialBannerAd(AdType.VIDEO) }
-        btnShowAd.setOnClickListener { showAd() }
-        btnShowAd.isEnabled = true
-    }
 
-    private fun requestInterstitialBannerAd(type: AdType) {
-        val options = TapsellAdRequestOptions(CACHE_TYPE_STREAMED)
-        Tapsell.requestAd(this@InterstitialActivity,
-            if (type == AdType.BANNER) "610ed8bc35114c6ff3a596ee" else
-                "610ecc7d260bc85635a14601", options,
-            object : TapsellAdRequestListener() {
-                override fun onAdAvailable(ad: TapsellAd?) {
-                    if (isDestroyed)
-                        return
-
-                    this@InterstitialActivity.ad = ad
-                    btnShowAd.isEnabled = true
-                }
-
-                override fun onExpiring(ad: TapsellAd?) {
-                    TODO("not implemented")
-                }
-
-                override fun onNoAdAvailable() {
-                    TODO("not implemented")
-                }
-
-                override fun onError(str: String?) {
-                    TODO("not implemented")
-                }
-
-                override fun onNoNetwork() {
-                    TODO("not implemented")
-                }
-            })
-    }
-
-    private fun showAd() {
-        ad?.let {
-            val showOptions = TapsellShowOptions()
-            showOptions.rotationMode = TapsellShowOptions.ROTATION_LOCKED_LANDSCAPE
-            it.show(this@InterstitialActivity, showOptions, object : TapsellAdShowListener() {
-                override fun onOpened(ad: TapsellAd) {
-                    Log.e("InterstitialActivity", "on ad opened")
-                }
-
-                override fun onClosed(ad: TapsellAd) {
-                    Log.e("InterstitialActivity", "on ad closed")
-                }
-            })
-        } ?: run {
-            Log.e("InterstitialActivity", "ad is not available")
-        }
-
-        btnShowAd.isEnabled = false
-        ad = null
-    }
-}
-
-enum class AdType {
-    BANNER, VIDEO
 }
